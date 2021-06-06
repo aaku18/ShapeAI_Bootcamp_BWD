933b2799c524a3d009ccdabb5939d55475c86bf1
aaku18 <77617049+aaku18@users.noreply.github.com>
2021-06-06 22:30:38 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha We covered
+        everything from Scratch including Javascript React.js, HTML. WE loved his teaching.thankyou so much.
+      </p>
+    </div>
+  );
+}
